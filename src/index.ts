945ca36a7f85f75d8f5afd501c80e1d4a919952a
Richard Roché <richard.roche@investec.co.za>
2018-07-01 17:56:34 +0200

    chore: add a koa app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import * as Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(3000);
+
+// console.log('Koa application is up and running on port 3000');
